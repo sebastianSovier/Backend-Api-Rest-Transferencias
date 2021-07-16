a55e81acd2dcd4eb51abbcc232a9e5dee119ce84
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2021-07-15 22:28:03 -0400

    cors

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ var corsOptions = {
   origin: 'https://angular-app-transferencias.herokuapp.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -31,14 +38,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({ 'message': err.message });
   return;
 });
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
