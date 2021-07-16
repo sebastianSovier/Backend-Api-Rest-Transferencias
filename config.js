de1dec572eb8aa9dbd7cba568c5a44df2f329fe2
sebastian sovier <sebastian.sovier@tgtgroup.cl>
2021-07-15 20:05:27 -0400

    seteo de bd

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const env = process.env;
 
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'root',
-    port:env.port || 3308,
-    password: env.DB_PASSWORD || 'sebsov15',
-    database: env.DB_NAME || 'bdTransacciones',
+    host: env.DB_HOST || 'sql10.freemysqlhosting.net',
+    user: env.DB_USER || 'sql10425613',
+    port:env.port || 3306,
+    password: env.DB_PASSWORD || 'qnx36bRF4Y',
+    database: env.DB_NAME || 'sql10425613',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
   secret:'secretkey'
